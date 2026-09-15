42f16fc6957492e6227b7cc86c070d0d6f673b4d
ahmad-56 <ahmad392.hassan@gmail.com>
2026-09-15 20:12:48 +0500

    2015 Day 9 Completed

---
+++ 2015/Day 9/exp1.py
@@ -0,0 +1,40 @@
+import math
+from pathlib import Path
+from itertools import permutations
+
+input_file = Path(r"C:\Users\snoor\OneDrive\Desktop\Python\AdventofCode\2015\Day 9\sample_input.txt")
+
+with input_file.open("r") as file:
+    user_input = file.readlines()
+
+locations = []
+array = []
+lines = user_input
+for line in lines:
+    line = line.split()
+    loc1 = line[0]
+    loc2 = line[2]
+    if loc1 not in locations:
+        locations.append(loc1)
+    if loc2 not in locations:
+        locations.append(loc2)
+    dist = line[-1]
+
+    array.append([loc1,loc2,dist])
+
+routes = permutations(locations)
+
+total = [] 
+for route in routes:
+    distance = 0
+    for i in range(len(route) - 1):
+        location = route[i]
+        next = route[i + 1]
+
+        for s in array:
+            if location in s and next in s:
+                distance += int(s[-1])
+                break
+    total.append(distance)
+
+print(f"Minimum Distance: {min(total)}")
